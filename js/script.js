aed31ef8ddfcc530a387807d9b05f0200aadfb67
aliraza401 <alirazaunir401@gmail.com>
2020-08-21 20:15:31 +0500

    about page coutup completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 
 // document.getElementById("is-active").style.cssText = "box-shadow: 0px -5px"; 
 
+
+jQuery(document).ready(function ($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+});
