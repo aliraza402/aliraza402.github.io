5258afd30a77e9e72855c0921a8fae453e65fc60
aliraza401 <alirazaunir401@gmail.com>
2020-08-22 13:24:27 +0500

    :up: completed site v1.0 - spaceing NOT

--- js/script.js
+++ js/script.js
@@ -2,10 +2,18 @@
 // document.getElementById("is-active").style.cssText = "box-shadow: 0px -5px"; 
 
 
-jQuery(document).ready(function ($) {
+
+var executed = false;
+if (!executed) {
+    executed = true;
     $('.counter').counterUp({
         delay: 10,
         time: 1000
     });
+    console.log('running counter')
+}else{
+    console.log('NOT running counter')
+}
 
-});
+
+
